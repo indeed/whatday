0bef111f9716972afd18e1a644a29166db90a022
Shun Yu Rao <shunyu.rao@gmail.com>
2015-09-28 15:13:43 -0400

    Aesthetically pleasing page

--- js/app.js
+++ js/app.js
@@ -3,11 +3,10 @@ var app = angular.module('whatday', ['whatday.services'])
 
 app.controller('mainCtrl', function ($scope, calendarService) {
     
-    $scope.day = {};
+    $scope.cycleDay = "";
 
     calendarService.getDayEvents(moment()).then(function (response) {
         // Display current day in cycle
-        $scope.cycleDay = {};
         $scope.offset = 0;
         calendarService.getDayEvents(moment().add($scope.offset, "days")).then(function (response) {
             var events = calendarService.parseEvents(response);
