82e5d897905880f69dc0a93bbffad831fde49f54
Shun Yu Rao <shunyu.rao@gmail.com>
2015-10-15 10:43:15 -0400

    Add display for next school day

--- js/app.js
+++ js/app.js
@@ -4,15 +4,29 @@ var app = angular.module('whatday', ['whatday.services'])
 app.controller('mainCtrl', function ($scope, calendarService) {
     
     $scope.cycleDay = "";
+    $scope.nextWeekDay = "";
+    $scope.nextDay = "";
 
-    calendarService.getDayEvents(moment()).then(function (response) {
-        // Display current day in cycle
-        $scope.offset = 0;
-        calendarService.getDayEvents(moment().add($scope.offset, "days")).then(function (response) {
-            var events = calendarService.parseEvents(response);
-            $scope.cycleDay = calendarService.getCycleDay(events);
-        });
+    calendarService.getDayEvents(moment().add(0, "days")).then(function (response) {
+        var events = calendarService.parseEvents(response);
+        $scope.cycleDay = calendarService.getCycleDay(events);
+        getNextDay(1);
     });
+
+    function getNextDay(offset) {
+        calendarService.getDayEvents(moment().add(offset, "days")).then(function (response) {
+            var events = calendarService.parseEvents(response);
+            if (calendarService.getCycleDay(events) == "No school") {
+                return getNextDay(offset + 1);
+            } else {
+                $scope.nextWeekDay = moment().add(offset, "days").format("dddd");
+                $scope.nextDay = calendarService.getCycleDay(events);
+            }
+        });
+    }
+    
+
+
 
 
 });
